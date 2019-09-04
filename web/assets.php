76f9760eab56a4234c1467730939880aa98e787e
Jan Petersen <jdpdo@outlook.de>
2019-09-04 12:30:09 +0200

    Add 'assets.php' and further configuration files.

---
+++ web/assets.php
@@ -0,0 +1,92 @@
+<?php 
+/**
+ * Provides functions and global environment variables needed for excecution and using of libaries and modules.
+ * 
+ * @author JDPDO
+ */
+
+/* Init global variables. */
+// Init path values
+$_GLOBAL['path_root'] = dirname(__DIR__);
+$_GLOBAL['path_assets'] = $_GLOBAL['path_root'] . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR;
+$_GLOBAL['path_config'] = $_GLOBAL['path_root'] . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR;
+$_GLOBAL['path_lib'] = $_GLOBAL['path_root'] . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR;
+$_GLOBAL['path_modules'] = $_GLOBAL['path_root'] . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR;
+
+/**
+ * Enumeration containing configuration types.
+ */
+class CONFIG_TYPES{ const INI = 0; const JSON = 1; }
+
+/**
+ * Provides key value pairs of configuration file.
+ * 
+ * @todo Return nested arrays, if files configuration content is object like (i.e.: JSON format).
+ * 
+ * @param string $name Name of the configuration file to use.
+ * @param CONFIG_TYPES $type Configuration type of the file.
+ * 
+ * @return array
+ */
+function parse_conifg($name, $type = CONFIG_TYPES::INI){
+    $file_path = $_GLOBAL['path_config'] . $name;
+    $file_configuration;
+    switch($type){
+        default:
+        case CONFIG_TYPES::INI:
+            $file_configuration = parse_ini_file($file_path);
+            break;
+        case CONFIG_TYPES::JSON:
+            throw new Exception('Not implemented yet.'); 
+            break;
+    }
+}
+
+/**
+ * Includes all libary files defined in *lib.ini*.
+ * 
+ * @return void
+ */
+function include_libs(){
+    $item_array = array_diff(scandir($_GLOBAL['path_lib']), array('..', '.'));
+    foreach($item_array as $item){
+        include_asset($item);
+    }
+}
+
+/**
+ * Includes an libary.
+ * 
+ * @param string $name Name of the libary file to include.
+ * 
+ * @return void
+ */
+function include_asset($name)
+{
+    include($_GLOBAL['path_lib'] . $name);
+}
+
+/**
+ * Includes all modules defined in *modules.ini*.
+ * 
+ * @return void
+ */
+function include_modules(){
+    $item_array = array_diff(scandir($_GLOBAL['path_modules']), array('..', '.'));
+    foreach($item_array as $item){
+        include_asset($item);
+    }
+}
+
+/**
+ * Includes an module.
+ * 
+ * @param string $name Name of the file or directory to include.
+ * 
+ * @return void
+ */
+function include_module($name)
+{
+    include($_GLOBAL['path_modules'] . $name);
+}
+?>
